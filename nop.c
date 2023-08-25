a7de939006a84222400ad9327bccf47918b2db3c
onokajames96 <jamesonoka996@gmail.com>
2023-08-25 02:03:47 -0700

    nop opcode

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ *nop - does not perfom any action.
+ *@stack: Double pointer to head of stack
+ * @line_number: the number of a line.
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+
+	(void)line_number;
+}
